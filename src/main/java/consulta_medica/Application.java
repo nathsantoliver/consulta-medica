d65544987c5cf410b7b92b5c5f012474c5186c7c
Nathália Oliveira <nathaliaolisantana@gmail.com>
2024-06-28 19:51:13 -0300

    changing name of main application

--- src/main/java/consulta_medica/Application.java
+++ src/main/java/consulta_medica/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConsultaMedicaApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConsultaMedicaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
